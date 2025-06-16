78505be3ece3e9b3cf27b0f9b62fef3d98e67c1f
Marcin <daxterxplx@gmail.com>
2025-06-16 02:36:41 +0200

    refactor: update prompts and adjust podcast LLM logic

--- src/logic/llm_podcast.py
+++ src/logic/llm_podcast.py
@@ -12,11 +12,12 @@ llm = AzureChatOpenAI(
     api_version=os.getenv("API_VERSION"),
     deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
     model_name=os.getenv("AZURE_OPENAI_MODEL"),
-    temperature=0.7
+    temperature=0.7,
+    max_tokens=8000
 )
 
 PROMPT_PATHS = {
-    "scientific": Path("prompts/scientific_style.txt"),
+    "scientific": Path("src/prompts/scientific_style.txt"),
     "casual": Path("src/prompts/casual_style.txt"),
 }
 
@@ -26,12 +27,25 @@ def load_prompt_template(style: str) -> PromptTemplate:
 
 def generate_podcast_text(style: str, input_text: str) -> str:
     prompt_template = load_prompt_template(style)
-    prompt = prompt_template.format(input_text=input_text)
-    response = llm.invoke(prompt)
+    user_prompt = prompt_template.format(input_text=input_text)
+
+    system_prompt = (
+        "Jesteś Agentem Podcastu Naukowego. Tworzysz klarowne, edukacyjne podcasty w stylu wykładowym."
+        if style == "scientific"
+        else "Jesteś Agentem Podcastu Hobbistycznego. Opowiadasz lekko, ciekawie i nieformalnie jak przy kawie."
+    )
+    print(user_prompt)
+    response = llm.invoke([
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt}
+    ])
     return response.content
 
+
 if __name__ == "__main__":
-    output = generate_podcast_text("scientific", "Podcast o słoniu po polsku")
+    
+    input_text = Path("src/logic/llm_text_test_file.txt").read_text(encoding="utf-8")
+    output = generate_podcast_text("scientific", input_text)
     print(output)
     # print(os.getenv("API_VERSION"))
     # print(os.getenv("AZURE_OPENAI_DEPLOYMENT"))
